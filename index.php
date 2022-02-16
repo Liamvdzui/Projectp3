5543f4acdded499168d9c4435e9de0b81abd7a12
korné <k.rijninks@gmail.com>
2022-02-16 15:47:35 +0100

    responsive navbar

--- index.php
+++ index.php
@@ -5,23 +5,27 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
 
     <title>Voetbal | Nieuws</title>
 </head>
 
 <body>
-    <div class="back"></div>
-    <nav>
-        <a href="#">About</a>
-        <a href="#">Works</a>
-        <a href="#">Partners</a>
-        <a href="#">Price</a>
-        <a href="#">Contacts</a>
-    </nav>
-
-
-
+    
+<nav class="navbar">
+      <div class="navbar-container container">
+          <input type="checkbox" name="" id="">
+          <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+          </div>
+          <ul class="menu-items">
+              <li><a href="#home">Home</a></li>
+              <li><a class="right"href="#contact">login</a></li>
+          </ul>
+      </div>
+</nav>
 </body>
 
 
