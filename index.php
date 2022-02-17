76848130adecefe4610bd6fd40d74f487d2bffab
korné <k.rijninks@gmail.com>
2022-02-17 13:24:16 +0100

    cards en footer

--- index.php
+++ index.php
@@ -6,37 +6,154 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="style.css">
+    <!-- wordt alleen gebruikt voor incons in footer -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Voetbal | Nieuws</title>
 </head>
 
 <body>
-    
 
-<div class="header">
-<nav class="navbar">
-      <div class="navbar-container container">
-          <input type="checkbox" name="" id="">
-          <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-          </div>
-          <ul class="menu-items">
-              <li><a href="#home">Home</a></li>
-              <li><a class="right"href="#contact">login</a></li>
-          </ul>
-      </div>
-</nav>
-  <div class="info">
-    <h1>KEN BURNS HEADERS ARE THE BEST</h1>
-    <div class="meta">
-      <a  href="https://twitter.com/nodws" target="_b" class="author"></a><br>
-      By <a href="https://twitter.com/nodws" target="_b">James Nodws</a> on May 30, 2019
+
+    <div class="header">
+        <nav class="navbar">
+            <div class="navbar-container container">
+                <input type="checkbox" name="" id="">
+                <div class="hamburger-lines">
+                    <span class="line line1"></span>
+                    <span class="line line2"></span>
+                    <span class="line line3"></span>
+                </div>
+                <ul class="menu-items">
+                    <li><a href="#home">Home</a></li>
+                    <li><a class="right" href="#contact">login</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="info">
+            <h1>Voetbal | Nieuws</h1>
+            <div class="meta">
+                <img class="author" src="./original.jpg" alt=""><br>
+                door Korné & liam</a>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-</body>
 
+    <div class="cards">
+        <div class="card">
+            <div class="card-header">
+                <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+            </div>
+            <div class="card-body">
+                <span class="tag tag-teal">Technology</span>
+                <h4>
+                    Why is the Tesla Cybertruck designed the way it
+                    is?
+                </h4>
+                <p>
+                    An exploration into the truck's polarising design
+                </p>
+                <div class="user">
+                    <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo"
+                        alt="user" />
+                    <div class="user-info">
+                        <h5>July Dec</h5>
+                        <small>2h ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <img src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg"
+                    alt="ballons" />
+            </div>
+            <div class="card-body">
+                <span class="tag tag-purple">Popular</span>
+                <h4>
+                    How to Keep Going When You Don’t Know What’s Next
+                </h4>
+                <p>
+                    The future can be scary, but there are ways to
+                    deal with that fear.
+                </p>
+                <div class="user">
+                    <img src="https://lh3.googleusercontent.com/ogw/ADGmqu8sn9zF15pW59JIYiLgx3PQ3EyZLFp5Zqao906l=s32-c-mo"
+                        alt="user" />
+                    <div class="user-info">
+                        <h5>Eyup Ucmaz</h5>
+                        <small>Yesterday</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <img src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg" alt="city" />
+            </div>
+            <div class="card-body">
+                <span class="tag tag-pink">Design</span>
+                <h4>
+                    10 Rules of Dashboard Design
+                </h4>
+                <p>
+                    Dashboard Design Guidelines
+                </p>
+                <div class="user">
+                    <img src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg" alt="user" />
+                    <div class="user-info">
+                        <h5>Carrie Brewer</h5>
+                        <small>1w ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <img src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg" alt="city" />
+            </div>
+            <div class="card-body">
+                <span class="tag tag-pink">Design</span>
+                <h4>
+                    10 Rules of Dashboard Design
+                </h4>
+                <p>
+                    Dashboard Design Guidelines
+                </p>
+                <div class="user">
+                    <img src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg" alt="user" />
+                    <div class="user-info">
+                        <h5>Carrie Brewer</h5>
+                        <small>1w ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+        <footer>
+            <div class="footer-content">
+                <h3>Voetbal Nieuws</h3>
+                <p>Voetbal Nieuws is een website met het aller nieuwste voetbal nieuws.<br> Login om het archief en meer nieuws te zien.</p>
+                <ul class="socials">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                </ul>
+            </div>
+            <div class="footer-bottom">
+                <p>copyright &copy; <a href="#">Korné & Liam</a> </p>
+                <div class="footer-menu">
+                    <ul class="f-menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </footer>
+</body>
 
 </html>
