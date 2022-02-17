0ffd1643dbad55b955a2688cbad832d6fa9eafb1
korné <k.rijninks@gmail.com>
2022-02-17 09:51:43 +0100

    header

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
 
     <title>Voetbal | Nieuws</title>
 </head>
 
 <body>
     
+
+<div class="header">
 <nav class="navbar">
       <div class="navbar-container container">
           <input type="checkbox" name="" id="">
@@ -26,6 +28,14 @@
           </ul>
       </div>
 </nav>
+  <div class="info">
+    <h1>KEN BURNS HEADERS ARE THE BEST</h1>
+    <div class="meta">
+      <a  href="https://twitter.com/nodws" target="_b" class="author"></a><br>
+      By <a href="https://twitter.com/nodws" target="_b">James Nodws</a> on May 30, 2019
+    </div>
+  </div>
+</div>
 </body>
 
 
